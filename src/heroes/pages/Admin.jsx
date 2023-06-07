1e026a9f2eeeb293f93a1c82749dfaea93770b70
Cristian Bernachi <a21110180@ceti.mx>
2023-06-06 21:31:52 -0600

    Agregué formulario HTML

--- src/heroes/pages/Admin.jsx
+++ src/heroes/pages/Admin.jsx
@@ -41,10 +41,38 @@ export const Admin = () => {
 
             <>
                    
-                {/* Aquí va el código HTML */}
+                   <h2>Agregar un nuevo comic de superhéroes</h2>
+            <form>
 
-                <h1 className="text-center">Hola xd</h1>
+                <label for="autor">Autor:</label>
+                <input type="text" id="autor" name="autor" required></input>
 
+                <label for="fecha_lanzamiento">Fecha de Lanzamiento:</label>
+                <input type="number" id="fecha_lanzamiento" name="fecha_lanzamiento" required></input>
+
+                <label for="titulo">Título del Comic:</label>
+                <input type="text" id="titulo" name="titulo" required></input>
+
+                <label for="identidad_secreta">Identidad Secreta:</label>
+                <input type="text" id="identidad_secreta" name="identidad_secreta" required></input>
+
+                <label for="primera_aparicion">Primera Aparición:</label>
+                <input type="text" id="primera_aparicion" name="primera_aparicion" required></input>
+
+                <label for="nombre_heroe">Nombre de Héroe:</label>
+                <input type="text" id="nombre_heroe" name="nombre_heroe" required></input>
+
+                <label for="anteriores_identidades">Anteriores Identidades Secretas:</label>
+                <input type="text" id="anteriores_identidades" name="anteriores_identidades" required></input>
+
+                <label for="id">Id heroe:</label>
+                <input type="text" id="id" name="id" required></input>
+
+                <label for="publisher">Publisher:</label>
+                <input type="text" id="publisher" name="publisher" required></input>
+
+                <input type="button" value="Guardar"></input>
+            </form>
             </>
 
         ) : (
